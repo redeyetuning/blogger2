4cda10cacd80f5cc8e0cc08a6814d27f6ae7eb6a
Samuel Masters <redeyetuning@gmail.com>
2018-02-06 16:11:20 +0400

    Tagging feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
 include ArticlesHelper
+  def index
+    @articles = Article.all
+  end
   
   def show
   	@article = Article.find(params[:id])
@@ -16,12 +19,28 @@ include ArticlesHelper
   def create
   	@article = Article.new(article_params)
   	@article.save
+    flash.notice = "Article '#{@article.title}' Created!"
   	redirect_to article_path(@article)
   end
+
+  def edit
+  @article = Article.find(params[:id])
+  end
+
+  def update
+  @article = Article.find(params[:id])
+  @article.update(article_params)
+
+  flash.notice = "Article '#{@article.title}' Updated!"
+
+  redirect_to article_path(@article)
+  end
+
 
   def destroy
    @article = Article.find(params[:id]) 
    @article.destroy
+   flash.notice = "Article Deleted!"
    redirect_to articles_path
   end
 
